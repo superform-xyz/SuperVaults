b778611b8d37e00db7de424b3bb4b1cca4b6647b
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-10-30 17:24:33 +0200

    fix: rm unused errors

--- src/ISuperVaultFactory.sol
+++ src/ISuperVaultFactory.sol
@@ -18,9 +18,6 @@ interface ISuperVaultFactory {
     /// @notice Error thrown when array lengths do not match
     error ARRAY_LENGTH_MISMATCH();
 
-    /// @notice Error thrown when invalid weights are provided
-    error INVALID_WEIGHTS();
-
     /// @notice Error thrown when the number of superforms is zero
     error ZERO_SUPERFORMS();
 
@@ -33,14 +30,8 @@ interface ISuperVaultFactory {
     /// @notice Error thrown when the final superform IDs array is empty
     error EMPTY_FINAL_SUPERFORM_IDS();
 
-    /// @notice Error thrown when a superform does not support the asset
-    error SUPERFORM_DOES_NOT_SUPPORT_ASSET();
-
     /// @notice Error thrown when the block chain ID is out of bounds
     error BLOCK_CHAIN_ID_OUT_OF_BOUNDS();
-
-    /// @notice Error thrown when a superform does not exist
-    error SUPERFORM_DOES_NOT_EXIST(uint256 superformId);
 
     //////////////////////////////////////////////////////////////
     //                          EVENTS                          //
@@ -70,13 +61,10 @@ interface ISuperVaultFactory {
 
     /// @notice Returns the data for a SuperVault
     /// @param superVault_ Address of the SuperVault
-    /// @return Data for the SuperVault
-    function getSuperVaultData(address superVault_) external view returns (ISuperVault.SuperVaultStrategyData memory);
-
-    /// @notice Returns the asset for a SuperVault
-    /// @param superVault_ Address of the SuperVault
-    /// @return Address of the asset
-    function getSuperVaultAsset(address superVault_) external view returns (address);
+    /// @return numberOfSuperforms The number of Superforms
+    /// @return superformIds Array of Superform IDs
+    /// @return weights Array of weights for each Superform
+    function getSuperVaultData(address superVault_) external view returns (uint256 numberOfSuperforms, uint256[] memory superformIds, uint256[] memory weights);
 
     /// @notice Returns the Superform IDs for a SuperVault
     /// @param superVault_ Address of the SuperVault
